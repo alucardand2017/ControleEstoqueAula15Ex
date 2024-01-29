11eafa329a1dc611eff98682abbc2869a0f41f84
ANDERSON SILVA <alucardand2017@gmail.com>
2024-01-28 21:19:22 -0300

    Teste de commit pelo VS code

---
+++ Models/CategoriaModel.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AULA15CONTROLEESTOQUE.Models{
+    public class CategoriaModel
+    {
+        [Key]
+        public int IdCategoria { get; set; }
+        [Required, MaxLength(128)]
+        public string Nome { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,9 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AULA15CONTROLEESTOQUE.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,10 +15,17 @@ namespace AULA15CONTROLEESTOQUE
 {
     public class Startup
     {
-        
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration){
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            services.AddDbContext<EstoqueWebContext>(option=>
+            option.UseSqlite(Configuration.GetConnectionString("EstoqueWebContext")));
         }
 
        
